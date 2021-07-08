f1a96c7aed1d09abcc46f6d25c3f3b4d2d703679
LeoProko <leoproko126@gmail.com>
2021-07-08 18:12:06 +0300

    [customer_app] remove login request from landing page

--- customer_app/handlers/index.py
+++ customer_app/handlers/index.py
@@ -6,8 +6,6 @@ from django.shortcuts import redirect
 from frontend_server.html_factories.base import BaseHtmlFactory
 from frontend_server.decorators import allowed_users
 
-@login_required(login_url='login')
-@allowed_users(allowed_users_list=[])
 def index(request):
     # template = Template(BaseHtmlFactory.create.customer_app('Ensio', 'customer_app/templates/', 'main', '', ''))
     # context = Context({
